569e611dd6eddd2079c2687f6ae25f3e23308018
joduckyong <dyddl7609@gmail.com>
2022-08-18 17:05:45 +0900

    공통 에러 메세지 추가

---
+++ src/main/java/kr/or/lx/common/RestTemplateErrorHandler.java
@@ -0,0 +1,34 @@
+package kr.or.lx.common;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Component
+public class RestTemplateErrorHandler implements ResponseErrorHandler {
+	
+	@Override
+	public boolean hasError(ClientHttpResponse response) throws IOException {
+		return response.getStatusCode().series() == HttpStatus.Series.CLIENT_ERROR || response.getStatusCode().series() == HttpStatus.Series.SERVER_ERROR;
+	}
+
+	@Override
+	public void handleError(ClientHttpResponse response) throws IOException {
+		log.error("{}", response.getStatusText());
+		
+		if(response.getStatusCode().is4xxClientError()) {
+			return;
+		}
+		
+		if(response.getStatusCode().is5xxServerError()){
+			return;
+		}
+	}
+}
