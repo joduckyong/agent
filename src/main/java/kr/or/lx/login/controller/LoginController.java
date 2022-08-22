3ad93f55ee9c41ca2d07c4662ff9f0c05c444615
LAPTOP-AHDO2SSJ\kang <khighbig@gmai.com>
2022-08-22 23:03:40 +0900

    로그인 화면 폼

---
+++ src/main/java/kr/or/lx/login/controller/LoginController.java
@@ -0,0 +1,35 @@
+package kr.or.lx.login.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.lx.common.ApiService;
+import lombok.extern.slf4j.Slf4j;
+
+
+@RequestMapping("login")
+@Slf4j
+@Controller
+public class LoginController {
+	
+    @Value("${agent.api.url}")
+    private String agentApiUrl;    
+	
+    @Autowired
+	private ApiService<?> apiService;
+	
+	/**
+     * 로그인 화면
+     * @return
+     */	
+	@GetMapping("/loginForm")
+	public String collectionMethodList(ModelMap model) throws Exception{
+		
+		return "login/login";
+	}
+	
+}
