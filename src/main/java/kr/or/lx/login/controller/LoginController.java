d25d14920fea5d4b4d85e4937f042f7d177a36f2
Kang <khighbig@gmail.com>
2022-08-24 10:12:50 +0900

    관리자 등록 추가

--- src/main/java/kr/or/lx/login/controller/LoginController.java
+++ src/main/java/kr/or/lx/login/controller/LoginController.java
@@ -1,11 +1,17 @@
 package kr.or.lx.login.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.or.lx.common.ApiService;
 import lombok.extern.slf4j.Slf4j;
@@ -41,5 +47,25 @@ public class LoginController {
 //		
 //		return "login/login";
 //	}
+	
+	@GetMapping("/admin/add")
+	public String adminAdd(Map<String, Object> param, ModelMap model) throws Exception{
+		
+		model.put("clct_id", param.get("clct_id"));	
+		
+		return "login/add";
+	}	
+	
+	@ResponseBody
+	@PostMapping("/admin/{apiId}")
+	public Object adminApi(@RequestBody Map<String, Object> param, ModelMap model) throws Exception{
+		
+		String url = agentApiUrl+param.get("url");
+		
+		ResponseEntity<?> responseEntity = apiService.post(url, param);
+		Object object = responseEntity.getBody();
+		
+		return object;
+	}
 	
 }
